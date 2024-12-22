7b2cd39ba1fd12732dd7ebb6536a69797ffae65d
Sayantan paul <paulsayantan96@gmail.com>
2024-12-23 00:49:12 +0530

    lint fix

--- scripts/resume.js
+++ scripts/resume.js
@@ -1,6 +1,5 @@
-
-document.getElementById('open-resume').addEventListener('click', (event) => {
-    event.preventDefault();
-    const resumeURL = './utils/documents/resume.pdf';
-    window.open(resumeURL, '_blank');
+document.getElementById("open-resume").addEventListener("click", (event) => {
+  event.preventDefault();
+  const resumeURL = "./utils/documents/resume.pdf";
+  window.open(resumeURL, "_blank");
 });
